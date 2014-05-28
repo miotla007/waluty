8c65d955fa034f8bab879e2784d23639c0831a0c
miotla007 <miotla007@wp.pl>
2014-05-28 15:36:05 +0200

    Dodalem Sortowanie

--- angular-seed/app/js/controllers.js
+++ angular-seed/app/js/controllers.js
@@ -5,10 +5,11 @@
 var myApp = angular.module('myApp.controllers', []);
 
 myApp.controller('WalutyListCtrl', function ($scope, $http) {
+	//var query = 'http://marketools.plus500.com/Feeds/UpdateTable?instsIds=56,55,2,100,27,5,177,305'
   $http.get('waluty/waluty.json').success(function(data) {
     $scope.waluty = data;
-    console.log(waluty);
+    //console.log(waluty);
   });
 
-  $scope.orderProp = 'I';
+  //$scope.orderProp = 'I';
 });
