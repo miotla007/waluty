1559809f155ca741317914d20a3a9ea7d5542818
miotla007 <miotla007@wp.pl>
2014-05-28 11:24:27 +0200

    Dodalem plik JSON oraz jego wyswietlanie

--- angular-seed/app/js/controllers.js
+++ angular-seed/app/js/controllers.js
@@ -2,11 +2,12 @@
 
 /* Controllers */
 
-var phonecatApp = angular.module('phonecatApp', []);
+var myApp = angular.module('myApp.controllers', []);
 
-phonecatApp.controller('WalutyListCtrl', function ($scope, $http) {
+myApp.controller('WalutyListCtrl', function ($scope, $http) {
   $http.get('waluty/waluty.json').success(function(data) {
     $scope.waluty = data;
+    console.log(waluty);
   });
 
   $scope.orderProp = 'I';
